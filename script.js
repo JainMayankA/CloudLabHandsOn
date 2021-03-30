bd5eb592a6998773e9bc664cfaf5ac6c27283b7a
Mayank Jain <maxjain71@gmail.com>
2021-03-29 20:36:28 -0500

    Simple Interest Calc.

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 function compute()
-{
-    p = document.getElementById("principal").value;
+{   
+    // input
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    //  Validation
+    if(principal>0){
+        // formula for interest
+    interest = principal*years*rate/100;
+    // get the future year
+    f_year = new Date().getFullYear() +  parseInt(years);
+
+    //  Display results 
+    document.getElementById("result").innerHTML = "If you deposit <mark>"+principal+" </mark>, <br> at an interest rate of <mark> "+rate+"</mark>.<br>You will receive an amount of <mark> "+interest+"</mark>,<br>in the year <mark> "+f_year+"</mark> ";
+        
+
+    }
+    if (principal<=0){
+        alert("Enter a positive number");
+    document.getElementById("principal").focus();
+    document.getElementById("principal").click();
+
+    }
+    
+
     
 }
-        
+
+// Update the current slider value (each time you drag the slider handle)
+function sliderLogic() {    
+    rate = document.getElementById("rate").value;
+    document.getElementById("op").innerHTML = rate + "%";
+}
+
+     
